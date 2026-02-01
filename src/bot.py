9d22cf315dceff48137d01874aa9ece8c59a7504
Joshua Burt <joshuaadburt@gmail.com>
2026-01-31 23:19:23 -0400

    Reverted some of wordle_streak pull request as some portions were outdated

--- src/bot.py
+++ src/bot.py
@@ -1,8 +1,3 @@
 import discord
 
-bot = None
-
-def create_bot():
-    global bot
-    bot = discord.Bot(intents=discord.Intents.all())
-    return bot
+bot = discord.Bot(intents=discord.Intents.all())
--- src/initialize.py
+++ src/initialize.py
@@ -5,7 +5,6 @@ from colorama import Fore
 
 
 # Local files
-import main_commands
 import roll
 import poll
 import gamble
